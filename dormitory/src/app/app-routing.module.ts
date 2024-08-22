3b80af69f0fc1c64c2d266043beed7699e17dde5
pongsaphak <bashooks00@gmail.com>
2024-08-23 00:58:20 +0700

    สร้างโปรเจค

---
+++ dormitory/src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Home1Component } from './home1/home1.component';
+
+const routes: Routes = [{ path: '', component: Home1Component }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ dormitory/src/app/home1/home1.component.ts
@@ -0,0 +1,27 @@
+import { Component, ViewChild } from '@angular/core';
+
+
+@Component({
+  selector: 'app-home1',
+  templateUrl: './home1.component.html',
+  styleUrls: ['./home1.component.css']
+})
+export class Home1Component {
+  header="https://s3-alpha-sig.figma.com/img/5dbf/70bc/e93e186ad21a65654e5d79f78d87c359?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=iR18MeN1KhbFiDrd7-64exaBZrTgcqm2qyn9a0Y8lMR-49cfK5VvhzhhHs4OVEiQ3rEtvyz6DB9nuwb14yGuNoeejpBETQ5CknNveUWmeTCkIKOa8tH8uxsy8cv0Bqq~03tc5IurS~iyteF-hPW7wO1qjrcAMcUTxJADmL4Yg0UoHpTP6V~dIoQpWzj~fTdOEJvo1X-a530SOEsWWbHrYrxD2ij2B1z0B8BATHIofLnnVVmN6oQeYUG2SEFEb48xSBgcsDqx0bv8brVTVnE0i1OTaC5duKBoW963kMNe3FZEo~6Hj9531yO21XAyQyfyp8QTqRo44Msgi0t8fcMvXg__"
+  footerimage = "https://s3-alpha-sig.figma.com/img/c676/1b1e/e36fb872b09125832952d247e27c8e7e?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Y1wesFswjDczDqSG03V38wHeI-zH9SBJyvnSZFkU9kaBmX-XXYdX43AQsuhM0NI8zyNjb8JvIp--wYBEe~n1CfkCwc3gb8CQW~cF5wYgg4XarX8p1ON68imZQJF1RvG0BeQR0OJ-nH0ev5HloZ5ypZ35J1UFV~dvtedMiM8Evl0QDKdzTttfODHSZNsCm4h1jalK~t8uxRGzKA43Je6aNo2bf12WNhOq~o5FPzulcNbF1ngUoH6nEySEjnO1NaP-yP1pNY93N37yLD3sdDFvUXiLBvXoMrnzD36XXiJrfIjxnmnbeUTPv0FSAkbaeEezaVBVuFSjE3LEcLDHSlu5tg__"
+  logo="https://s3-alpha-sig.figma.com/img/0e4f/3975/93d5b16382bf16776a56ef96938bf127?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=inTS12YyD~UhaHR9uXzLBNLMG96Si-oY0kAvBwEjnEwG2hHkWscio9hKGo28VcK93MkB8ZRWswU8~lBc~-TkEfMTOYi7VX0QU74UfK3OYftXx2hU9BcAxkdrSpkgzuiadoVVq8IMjGc-Qrnq3R5td5SQfz57ZWN9rrAiodUGQsVGoH3z9RxWbEPtZ~V97S6lIyjF9OU3Q-o1ZnxPyeDk0a2A8kQae56sRMeAZ7-UmYHZpvUWVF~EQ9EPbdfyWcyM6FxGBJGhwvxx2SkY4L8qrECIuatMCIuqrPLQI6oCLkPKIpPG-dQ7ZbyenU3vdoyumIYfGdc9KZUeRfkBThCjlQ__"
+
+
+  
+  }
+
+
+
+
+
+
+
+
+
+
+
